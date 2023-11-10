a7b18ceb32a00c200742b03b503015a676ce15ba
azambrao02 <a.zambrano02@ufromail.cl>
2023-11-09 22:09:26 -0300

    agregue un metodo para cambiar el correo electronico

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -6,18 +6,23 @@ public class Persona {
 	public String getNombre() {
 		return this.nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getRut() {
 		return this.rut;
 	}
+
 	public void setRut(String rut) {
 		this.rut = rut;
 	}
+
 	public String getCorreoElectronico() {
 		return this.correoElectronico;
 	}
+
 	public void setCorreoElectronico(String correoElectronico) {
 		this.correoElectronico = correoElectronico;
 	}
@@ -27,8 +32,13 @@ public class Persona {
 		this.rut = rut;
 		this.correoElectronico = correoElectronico;
 	}
+
 	@Override
 	public String toString() {
 		return "Nombre: " + nombre + ", Rut: " + rut + ", Correo Electronico: " + correoElectronico;
 
+	}
+	public void actualizarCorreoElectronico(String nuevoCorreoElectronico) {
+		this.correoElectronico = nuevoCorreoElectronico;
+	}
 }
